9ff3d8385c4601d1af0a360214dd9d870a2a652b
Adrian Garett C. Sian <adriansian@gmail.com>
2024-07-02 13:09:33 +0800

    add my learnings

---
+++ src/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+    public static void main(String[] args){
+    int studentAge = 23;
+    double studentGPA = 1.75;
+    char studentFirstInitial = 'A';
+    char studentLastInitial = 'S';
+    boolean hasPerfectAttendance = true;
+    String myName = "Adrian Sian";
+    String myFirstName = "Adrian";
+    String myLastName = "Sian";
+
+    System.out.println(studentAge);
+    System.out.println(myName);
+    System.out.println(myFirstName + " " + myLastName);
+
+    }
+}
